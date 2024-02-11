336e95a7f0fa03ed2e714d76a81c7286e3ce2d87
Sudipta Basak <sudiptab2100@gmail.com>
2024-02-11 18:25:03 +0530

    Renamed files

--- test/zkAuth.t.sol
+++ test/zkAuth.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {zkAuth} from "../src/zkAuth.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract zkAuthTest is Test {
+    zkAuth public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new zkAuth();
         counter.setNumber(0);
     }
 
