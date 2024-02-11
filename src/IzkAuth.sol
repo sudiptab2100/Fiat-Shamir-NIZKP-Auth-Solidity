953df9bab6d35c9253d691387fd7b32b8c33ec43
Sudipta Basak <sudiptab2100@gmail.com>
2024-02-12 03:29:23 +0530

    Update IzkAuth.sol

--- src/IzkAuth.sol
+++ src/IzkAuth.sol
@@ -6,6 +6,7 @@ interface IzkAuth {
     function getQ() external view returns(uint256 Q);
     function getG() external view returns(uint256 G);
     function get_k(address user) external view returns(uint256 k); // valid authentication count
+    function set_commitment(uint256 y) external; // set commitment of a user (Set Password)
     function get_commitment(address user) external view returns(uint256 y); // get commitment of a user
     function authenticate(address user, uint256 t, uint256 r) external returns(bool verified); // {t, r} is the proof
 }
